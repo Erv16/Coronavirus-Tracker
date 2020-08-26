c40b765f21583cabeb4507a2c6ab76428920f720
Erv16 <epalani1@binghamton.edu>
2020-08-26 02:52:20 -0400

    refactor controller to extract data from the service, append it to the model and pass the model onto the template and added template to display the data

--- coronavirus-tracker/src/main/java/com/palanierwin/coronavirustracker/services/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/com/palanierwin/coronavirustracker/services/CoronaVirusDataService.java
@@ -24,6 +24,10 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     private List<LocationStats> allStats = new ArrayList<>();
 
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     // @PostConstruct - after an instance of the service is created execute the
     // following method
     // @Scheduled schedules the run of the method for a specifc time. In this
@@ -47,7 +51,6 @@ public class CoronaVirusDataService {
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
             locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
-            System.out.println(locationStat.toString());
             newStats.add(locationStat);
         }
         this.allStats = newStats;
